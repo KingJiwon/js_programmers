a908e40a13e45fa88b81c6989cb3a039ff98a41c
KingJiwon <wldnjs0401@naver.com>
2023-02-09 00:14:07 +0900

    자연수 간격 배열 만들기 풀이

---
+++ lev1/08_natural_interval_list.js
@@ -0,0 +1,12 @@
+function solution(x, n) {
+  let answer = [];
+  for (let length = 0; length < n; length++) {
+    answer.push(x * i);
+  }
+  return answer;
+}
+
+//다른 사람의 풀이
+// function solution(x, n) {
+//     return Array(n).fill(x).map((v, i) => (i + 1) * v)
+// }
