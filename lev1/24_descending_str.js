c9a69582b910d6f307d527de38ef6ea22b8716db
KingJiwon <wldnjs0401@naver.com>
2023-04-24 23:46:46 +0900

    문자열 내림차순 정렬하기

--- lev1/24_descending_str.js
+++ lev1/24_descending_str.js
@@ -1,1 +1,16 @@
-function solution(s) {}
+function solution(s) {
+  const strArr = s.split("");
+  const asciiArr = strArr.map((el) => el.charCodeAt());
+  const sortedAsciiArr = asciiArr.sort((a, b) => b - a);
+  const sortedStrArr = sortedAsciiArr.map((el) => String.fromCharCode(el));
+  const sortedStr = sortedStrArr.join("");
+  return sortedStr;
+}
+
+// function solution(s) {
+//     return s
+//       .split("")
+//       .sort()
+//       .reverse()
+//       .join("");
+//   }
