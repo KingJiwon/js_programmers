bb0de40f0bcad652b2ce576c77c9bd31055d9672
KingJiwon <wldnjs0401@naver.com>
2023-06-09 15:55:24 +0900

    최대공약수와 최소공배수 구하기(유클리드 호제법)

--- lev1/30_common_factor_and_multiple.js
+++ lev1/30_common_factor_and_multiple.js
@@ -1,1 +1,16 @@
-function solution(n, m) {}
+function calcGcd(a, b) {
+  if (b === 0) {
+    return a;
+  }
+  return calcGcd(b, a % b);
+}
+
+function calcLcm(a, b, gcd) {
+  return (a * b) / gcd;
+}
+
+function solution(n, m) {
+  const gcd = calcGcd(n, m);
+  const lcm = calcLcm(n, m, gcd);
+  return [gcd, lcm];
+}
