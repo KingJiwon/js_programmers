12d41d42515b3ec1df88a0e694316ee452af0ad2
KingJiwon <wldnjs0401@naver.com>
2023-06-13 13:31:12 +0900

    배열의 중복 숫자 제거하기

--- lev1/31_duplicate_num.js
+++ lev1/31_duplicate_num.js
@@ -1,3 +1,11 @@
 function solution(arr) {
-  return answer;
+  const answerArr = [];
+  arr.map((el, idx) => {
+    if (el !== arr[idx + 1]) {
+      answerArr.push(el);
+    }
+  });
+  return answerArr;
 }
+
+console.log(solution([4, 4, 4, 3, 3]));
