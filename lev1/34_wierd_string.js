b3f933986342373d774eff0611c8ffd148c5a25f
KingJiwon <wldnjs0401@naver.com>
2024-05-05 17:50:11 +0900

    이상한 문자 만들기

--- lev1/34_wierd_string.js
+++ lev1/34_wierd_string.js
@@ -1,6 +1,15 @@
 function solution(s) {
-  const deletedBlank = s.split(" ").join("");
-
-  return deletedBlank;
+  const deletedBlank = s.split(" ").map((el) => el.split(""));
+  const newdelArr = deletedBlank.map((el) => {
+    const newArr = el.map((str, idx) => {
+      if (idx % 2 === 0) {
+        return str.toUpperCase();
+      } else {
+        return str.toLowerCase();
+      }
+    });
+    return newArr.join("");
+  });
+  return newdelArr.join(" ");
 }
 console.log(solution("try hello world"));
