49d5f9b0901acb0109f1f91f2b87025bb577323a
KingJiwon <wldnjs0401@naver.com>
2024-05-07 18:09:25 +0900

    예산

--- lev1/35_budget.js
+++ lev1/35_budget.js
@@ -1,4 +1,13 @@
 function solution(d, budget) {
-  var answer = 0;
-  return answer;
+  const sorted = d.sort((a, b) => a - b);
+  let purchaseNum = 0;
+  sorted.reduce((acc, cur) => {
+    if (acc - cur >= 0) {
+      purchaseNum += 1;
+      return acc - cur;
+    }
+  }, budget);
+  return purchaseNum;
 }
+
+solution([1, 3, 2, 5, 4], 9);
